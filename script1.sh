94faa0e1a6353b1e13173e9548e99c364452084e
edidiongakpan <edidiongkpan18@gmail.com>
2025-05-12 16:17:05 +0100

    script1

---
+++ script1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ #This a bashscript to check if a python3 packaage is available on my machine. if not available, it will automatically install the python package and ask the user if they want to run it.
+
+file=python
+
+if [ -f ~/$file ]
+
+then
+
+        echo "$file is already installed"
+
+else
+
+        echo "$file is not available!"
+        echo "installing $file..."
+        echo "Dont quit the command line"
+
+        sudo apt update && sudo apt install python3 -y
+
+        echo "$file succesfully installed"
+fi
+
+        read -p "Do you want to run $file now? (yes/no):" answer
+
+        if [[ "answer" == "yes" ]]
+then
+        $file
+else
+        echo
+
+fi
+
