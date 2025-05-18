535000bcdf7b6e3a355cd70db180075fe1e2a38b
Edidiong  Akpan <edidiongakpan18@gmail.com>
2025-05-18 21:31:53 +0100

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -2,7 +2,7 @@
 
  #This a Bashscript to check if a python3 packaage is available on a machine. if not available, it will automatically install the python package and ask the user if they want to run it.
 
-file=python
+file=python3
 
 if [ -f ~/$file ]
 
